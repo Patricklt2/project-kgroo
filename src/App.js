a28ee2aa4d06be3f5af75c2eb8ea161d58931379
Patricklt2 <ptorlaschis@gmail.com>
2024-01-07 20:05:42 -0300

    Small additions

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import './crossword/Crucigrama.css';
+import Crucigrama from "./crossword/Crucigrama";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-logo"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Santi puto
-        </a>
-      </header>
+        <button className="Button" onClick={Crucigrama}> Button</button>
+        <button className="Button"> Button</button>
     </div>
   );
 }
 
-function Crucigrama(){
-  return (
-   <div className="">
-
-   </div>
-  );
-}
-
-export default App;
+export default Crucigrama;
---
+++ src/crossword/Crucigrama.js
@@ -0,0 +1,14 @@
+function Crucigrama(){
+    return (
+        <>
+            <div className="Crucigrama">
+                <h1 className="CruciHead"> Bienvenido al Crucigrama!</h1>
+            </div>
+            <div className="Layout">
+
+            </div>
+        </>
+    );
+}
+
+export default Crucigrama;
